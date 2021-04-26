ecdfa0d1569aa0ecc3725f17f1aae61c1f626e4c
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-04-26 20:57:56 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -25,12 +25,6 @@ print(c[0])
 # The valid phone number program.
 # Make a program that checks if a string is in the right format for a phone number. The program should check that the string contains only numerical characters and is only 10 characters long. Print a suitable message depending on the outcome of the string evaluation.
 
-number_tel = input('Ведіть номер телефона,')
-while True:
-    if number_tel == len(number_tel>10)
-    
-
-
 # Task 3
 # The name check.
 # Write a program that has a variable with your name stored ( in lowercase) and then asks for your name as input. The program should check if your input is equal to the stored name even if the given name has another case, e.g., if your input is “Anton” and the stored name is “anton”, it should return True.
